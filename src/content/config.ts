e053d1dbf42b2a4a9ccca8c29827b1f53b643d52
Miko Ademagic <miko.ademagic@innowell.org>
2024-10-16 17:07:27 +1100

    remove blog from astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -88,18 +88,6 @@ const obj_pc = {
 	}),
 }
 
-const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date().optional(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
 const glossary = defineCollection({
 	type: 'data',
 	schema: z.object({
