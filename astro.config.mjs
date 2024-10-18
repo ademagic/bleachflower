5d07d48b2a72a3284a17186389f2c7cb684c0053
Miko Ademagic <miko.ademagic@innowell.org>
2024-10-18 16:00:18 +1100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx()],
+    site: 'https://example.com',
+    integrations: [mdx(), tailwind()],
 });
